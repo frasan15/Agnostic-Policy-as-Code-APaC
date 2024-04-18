eed9ca4c117d4fbe7c0605e2a8d6cf9741963df5
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-04-18 11:48:40 +0200

    adding remote host on main.tf - 9 attempt

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,43 +1,16 @@
-provider "null" {}
-
-resource "null_resource" "docker_installation" {
-  # Define connection details to the remote host
-  connection {
-    type        = "ssh"
-    host        = "10.212.174.49"
-    user        = "ubuntu"
-    private_key = file("/home/ubuntu/.ssh/id_rsa")  # Path to your private key
-    timeout     = "2m"
-  }
-
-  # Check if Docker is installed, if not, download and install Docker
-  provisioner "remote-exec" {
-    inline = [
-      "if ! command -v docker &> /dev/null; then",
-      "  curl -fsSL https://get.docker.com -o get-docker.sh",
-      "  sudo sh get-docker.sh",
-      "  sudo usermod -aG docker $USER",  # Add current user to the docker group
-      "  sudo systemctl enable docker",
-      "  sudo systemctl start docker",
-      "fi"
-    ]
-  }
+provider "docker" {
+  host = "tcp://10.212.174.49:2376"
 }
 
-resource "null_resource" "nginx_container" {
-  # Define connection details to the remote host
-  connection {
-    type        = "ssh"
-    host        = "10.212.174.49"
-    user        = "ubuntu"
-    private_key = file("/home/ubuntu/.ssh/id_rsa")  # Path to your private key
-    timeout     = "2m"
-  }
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+}
 
-  # Use remote-exec provisioner to run NGINX container
-  provisioner "remote-exec" {
-    inline = [
-      "sudo docker run -d --name nginx_container -p 80:80 nginx"
-    ]
+resource "docker_container" "nginx" {
+  name  = "nginx_container"
+  image = docker_image.nginx.latest
+  ports {
+    internal = 80
+    external = 80
   }
 }
