4ad1514a893d5289aa6f45a4d1710e409a68145e
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-04-18 16:13:01 +0200

    terraform instantiating OpenStack webserver - 7 attempt

--- terraform-openstack/main.tf
+++ terraform-openstack/main.tf
@@ -12,10 +12,26 @@ terraform {
 # Configure the OpenStack Provider
 provider "openstack" {
   user_name   = "frasant"
+  project_domain_id = "cb782810849b4ce8bce7f078cc193b19"
+  user_domain_name = "NTNU"
+  tenant_id   = "630a1bb146cd455f9dcea286cf2347a6"
   tenant_name = "TTM4905_V24_fransant"
   password    = var.openstack_password
   auth_url    = "https://api.skyhigh.iik.ntnu.no:5000"
   region      = "SkyHiGh"
+}
+
+# Define a network for the web server
+data "openstack_networking_network_v2" "web_server_network" {
+  name = "MySecondNetwork"
+  # Specify network details or customize as needed
+}
+
+# Define a security group for the web server
+data "openstack_networking_secgroup_v2" "web_server_secgroup" {
+  name        = "default"
+  description = "defaut security group"
+  # Define security group rules as needed
 }
 
 # Create a web server instance
@@ -23,8 +39,10 @@ resource "openstack_compute_instance_v2" "web_server" {
   name            = "web_server"
   flavor_name     = "gx1.2c4r"
   image_id        = "db1bc18e-81e3-477e-9067-eecaa459ec33"
-  network         = "MySecondNetwork"
-  security_groups = "default"
+  network {
+    uuid = data.openstack_networking_network_v2.web_server_network.id
+  }
+  security_groups = [data.openstack_networking_secgroup_v2.web_server_secgroup.name]
   # Customize additional instance configuration as needed
 
   # Example: Use cloud-init to configure the web server
