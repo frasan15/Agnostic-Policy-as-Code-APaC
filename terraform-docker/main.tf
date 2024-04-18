fb96bd58bb9a61d5cd9108b56141a4de2a7c721a
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-04-18 12:03:46 +0200

    adding remote host on main.tf - 13 attempt

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,18 +1,20 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "2.18.0" // Specify the version you want to use
-    }
+provider "docker" {
+  host = "tcp://10.212.174.49:2376"
+}
+
+# New resource to install Docker on the remote host
+resource "null_resource" "install_docker" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "ssh user@10.212.174.49 'sudo apt-get update && sudo apt-get install -y docker-ce'"
   }
 }
 
-provider "docker" {
-  host = "http://10.212.174.49:8000"
-}
-
 resource "docker_image" "nginx" {
-  name         = "nginx:latest"
+  name = "nginx:latest"
 }
 
 resource "docker_container" "nginx" {
