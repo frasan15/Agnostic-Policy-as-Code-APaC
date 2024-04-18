d6fc536106eb6cb1d26815311361461f07e4d1cc
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-04-18 11:49:58 +0200

    adding remote host on main.tf - 10 attempt

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.18.0" // Specify the version you want to use
+    }
+  }
+}
+
 provider "docker" {
   host = "tcp://10.212.174.49:2376"
 }
