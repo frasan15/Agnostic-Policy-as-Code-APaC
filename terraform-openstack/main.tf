7a170e38db11d1bae3170de80666c95ca9c047dd
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-04-19 16:32:37 +0200

    terraform instantiating OpenStack webserver - 42 attempt

--- terraform-openstack/main.tf
+++ terraform-openstack/main.tf
@@ -47,13 +47,17 @@ resource "openstack_compute_instance_v2" "web_server" {
       "sudo apt-get install -y apache2",
       "echo '<html><head><title>Welcome to My Custom Web Server</title></head><body><h1>Hello, world!</h1><p>This is a custom web server running on port 80.</p></body></html>' | sudo tee /var/www/html/index.html",
       "sudo systemctl restart apache2"
+      "mkdir -p ~/.ssh"
+      "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCM3bB7L7kesUSb0aXchVHtSoV29cWWDUO9z8C7vQkMD4atBuO407NAQ0Wbhn30twyz3UjwKckttvvPneNOlktCdj03IcVr7Endc1vxBpInCDkjWTcEJVLlGOjlLWrN5jto+fUckegyL5k80/ulYIlRlNnajId44LhKFpW6wjUlSLmFgglFAncBXzOit8piijAgCNCQLb7zukCmY55kuqeOV7yd2+OJ52UjkPbFdPh/kYgsPKY/flCSCODJqBB+3LIL/bZCEboHaZNRGi64EN+DIPiPg/qVG3l2oFqiBKQkaBC318KDdnblSY0BbEe9q2pdI/ck2tLal55rvyD9Ey2h8Fcy+TGgy9+7KTyu/ntym8mY+uNfK/OykU/eGTbn0E9WCHcpz73+jpCIeHwZMkDwi+KPL0gSkQBNSUoyMSf6kaURQ/LZgRgwMuKBj6twyCa9/8McVWvDvrZCq1so227erk3M5OPR2Xdsedvbkp8jtVQjLAbT9v1H7gJk90TG5vhy4YjpohyRU6d9DkchAQzVkXtFeeMXX5d6pFHit3F3cN/PefP8k2/y1CqxaN+mBHsBnm8ErrONiXM7W4ZcfK40Cum2LZ7wUS3MOyrUHD5cwwY++AKFjGeB+a99+I+4qP7/x49M5aKZq/WoIEt1kAi/Ls457UqlvixSQpeI+iX8kQ== ubuntu@mythirdserver' >> ~/.ssh/authorized_keys"
+      "chmod 700 ~/.ssh"
+      "chmod 600 ~/.ssh/authorized_keys"
     ]
 
     connection {
       type        = "ssh"
       user        = "ubuntu"  # Adjust the username based on your VM's operating system
       private_key = file("/home/ubuntu/.ssh/id_rsa")  # Adjust the path to your private key file
-      host        = "10.212.173.20"  # Use the fixed IP address of the instance
+      host        = self.network[0].fixed_ip_v4  # Use the fixed IP address of the instance
     }
   }
 
