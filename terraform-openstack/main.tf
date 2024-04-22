12cb172e4c00441e6dc6cae7d968dcaa087f16c8
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-04-22 12:33:32 +0200

    terraform instantiating vm with via OpenStack- trying to instantiate vm with public key already configured - 2 attempt

--- terraform-openstack/main.tf
+++ terraform-openstack/main.tf
@@ -47,3 +47,22 @@ resource "openstack_compute_floatingip_associate_v2" "myip" {
   instance_id = openstack_compute_instance_v2.web_server.id # this is the id of the instance to assoicate the floating ip with
   fixed_ip = openstack_compute_instance_v2.web_server.network.0.fixed_ip_v4 # the fixed ip address of the instance. This ensures that the floating IP is associated with the correct interface on the instance
 }
+
+# Connect to the machine using remote-exec provisioner
+resource "null_resource" "remote_exec" {
+  depends_on = [openstack_compute_floatingip_associate_v2.myip]
+  
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Connected to the instance!'"
+      # You can add more commands here to perform operations on the instance
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"  # Adjust the username based on your VM's operating system
+      private_key = file("/home/ubuntu/.ssh/id_rsa")  # Adjust the path to your private key file
+      host        = openstack_networking_floatingip_v2.myip.address  # Use the floating IP address of the instance
+    }
+  }
+}
