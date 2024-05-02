90eb4fae2ed7c0cba2cd3f7321f09640c8fbe84b
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-02 15:57:48 +0200

    trying to retrieve information details about network and security group - 11 attempt

--- ansible-openstack/python-parsing/playbook.py
+++ ansible-openstack/python-parsing/playbook.py
@@ -1,38 +1,8 @@
-from ansible import context
-from ansible.executor.playbook_executor import PlaybookExecutor
-from ansible.inventory.manager import InventoryManager
-from ansible.parsing.dataloader import DataLoader
-from ansible.vars.manager import VariableManager
-
-# Set the path to your playbook
-playbook_path = '/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack/playbook.yml'
-
-# Initialize Ansible context
-#context.CLIARGS = {}
-
-# You can then set specific command-line arguments as needed
-# For example, to set the verbosity level to 2:
-#context.CLIARGS['verbosity'] = 2
-
-# Initialize Ansible context
-context.CLIARGS['verbosity'] = 2  # Set verbosity level directly
-
-# Load data
-loader = DataLoader()
-inventory = InventoryManager(loader=loader, sources=['localhost'])
-variable_manager = VariableManager(loader=loader, inventory=inventory)
-
-# Create a playbook executor
-pbex = PlaybookExecutor(
-    playbooks=[playbook_path],
-    inventory=inventory,
-    variable_manager=variable_manager,
-    loader=loader,
-    passwords={},
-)
-
-# Run the playbook
-results = pbex.run()
-
-# Print the results
-print(results)
+import ansible_runner
+r = ansible_runner.run(private_data_dir='/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack', playbook='/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack/playbook.yml')
+print("{}: {}".format(r.status, r.rc))
+# successful: 0
+for each_host_event in r.events:
+    print(each_host_event['event'])
+print("Final status:")
+print(r.stats)
