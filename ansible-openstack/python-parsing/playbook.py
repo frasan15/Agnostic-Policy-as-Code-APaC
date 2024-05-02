8d013f6e5d6dd48a82aa0019ca069644260bd6fb
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-02 15:23:23 +0200

    trying to retrieve information details about network and security group - 5 attempt

---
+++ ansible-openstack/python-parsing/playbook.py
@@ -0,0 +1,31 @@
+from ansible import context
+from ansible.executor.playbook_executor import PlaybookExecutor
+from ansible.inventory.manager import InventoryManager
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+
+# Set the path to your playbook
+playbook_path = '/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack/playbook.yml'
+
+# Initialize Ansible context
+context.CLIARGS = {}
+
+# Load data
+loader = DataLoader()
+# inventory = InventoryManager(loader=loader, sources=['localhost'])
+# variable_manager = VariableManager(loader=loader, inventory=inventory)
+
+# Create a playbook executor
+pbex = PlaybookExecutor(
+    playbooks=[playbook_path],
+    #inventory=inventory,
+    #variable_manager=variable_manager,
+    loader=loader,
+    #passwords={},
+)
+
+# Run the playbook
+results = pbex.run()
+
+# Print the results
+print(results)
