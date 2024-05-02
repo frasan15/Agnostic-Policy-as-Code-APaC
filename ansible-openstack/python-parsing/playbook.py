24fa88c883ef0891dbe28fe37406cfd536083892
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-02 19:05:43 +0200

    trying to retrieve information details about network and security group - 20 attempt

--- ansible-openstack/python-parsing/playbook.py
+++ ansible-openstack/python-parsing/playbook.py
@@ -1,11 +1,35 @@
-# The following code is able to run the Ansible Playbook from this Python script
+from ansible.executor.playbook_executor import PlaybookExecutor
+from ansible.inventory.manager import InventoryManager
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
 
-import ansible_runner
-r = ansible_runner.run(private_data_dir='/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack', playbook='/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack/playbook.yml')
-print("{}", r.events)
-#print("{}: {}".format(r.status, r.rc))
-# successful: 0
-#for each_host_event in r.events:
- #   print(each_host_event['event'])
-#print("Final status:")
-#print(r.stats)
+# Set the path to your playbook
+playbook_path = '/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack/playbook.yml'
+
+# Initialize Ansible context
+loader = DataLoader()
+inventory = InventoryManager(loader=loader, sources='localhost')
+variable_manager = VariableManager(loader=loader, inventory=inventory)
+
+# Create a playbook executor
+pbex = PlaybookExecutor(
+    playbooks=[playbook_path],
+    inventory=inventory,
+    variable_manager=variable_manager,
+    loader=loader,
+)
+
+# Run the playbook
+results = pbex.run()
+
+# Access the registered variables
+network_details = variable_manager.get_vars()['network_details']
+security_group_details = variable_manager.get_vars()['security_group_details']
+
+# Print the network details
+print("Network details:")
+print(network_details)
+
+# Print the security group details
+print("Security group details:")
+print(security_group_details)
