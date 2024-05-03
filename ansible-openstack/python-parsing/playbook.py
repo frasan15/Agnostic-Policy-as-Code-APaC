ab7f35785c57c558b2ec1ba9522021a1835b1afa
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-03 14:57:35 +0200

    trying to retrieve information details about network and security group - 42 attempt

--- ansible-openstack/python-parsing/playbook.py
+++ ansible-openstack/python-parsing/playbook.py
@@ -2,13 +2,15 @@
 
 import ansible_runner
 r = ansible_runner.run(private_data_dir='/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack', playbook='/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack/playbook.yml')
-print("this is the format: {}: {}".format(r.status, r.rc))
-# successful: 0
+
+stdout_list = []  # Initialize an empty list to store stdout values
 for each_host_event in r.events:
-    print(each_host_event['event'])
-    if 'stdout' in each_host_event:
-        print(each_host_event['stdout'])
-    else:
-        print("No stdout for this event")
-print("Final status:")
-print(r.stdout)
+    if each_host_event['event'] == "runner_on_ok":
+        if 'stdout' in each_host_event:
+            stdout_list.append(each_host_event['stdout'])
+
+# Now stdout_list contains all stdout values corresponding to events with 'runner_on_ok'
+
+# Print each value in stdout_list
+for stdout_value in stdout_list:
+    print(stdout_value)
