db783243773f9b2e0be57ec6376eba7bd756838c
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-03 17:46:57 +0200

    trying to retrieve information details about network and security group - 104 attempt

--- ansible-openstack/python-parsing/playbook.py
+++ ansible-openstack/python-parsing/playbook.py
@@ -14,16 +14,14 @@ for each_host_event in r.events:
             print("value:", stdout_value)
             # Remove ANSI escape codes from the string
             clean_stdout_value = re.sub(r'\x1b\[[0-9;]*m', '', stdout_value)
-            print(repr(stdout_value))
             print("Cleaned value: ", repr(clean_stdout_value))
-            print(clean_stdout_value.find("2"))
-            print(clean_stdout_value.find("3"))
-            print(clean_stdout_value.find(";"))
-            print(clean_stdout_value.find("0"))
             print(clean_stdout_value.find("["))
             print(clean_stdout_value.find("msg"))
+            print(clean_stdout_value[0])
+            print(clean_stdout_value[1])
+            print(clean_stdout_value[2])
 
-            print(stdout_value.startswith("b[0;32mok: [localhost]"))
+            print(stdout_value.startswith("ok: [localhost]"))
             if stdout_value.startswith("ok: [localhost]"):  # Remove space after ":"
                 # Extract the object and remove the last '}'
                 object_start_index = stdout_value.find("{")
