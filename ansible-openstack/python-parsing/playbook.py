36eb9b06efb5a28fe42384d798752c9a853c8022
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-03 16:11:06 +0200

    trying to retrieve information details about network and security group - 75 attempt

--- ansible-openstack/python-parsing/playbook.py
+++ ansible-openstack/python-parsing/playbook.py
@@ -9,7 +9,7 @@ for each_host_event in r.events:
     if each_host_event['event'] == "runner_on_ok":
         if 'stdout' in each_host_event:
             stdout_value = each_host_event['stdout']
-            print("value: ", stdout_value)
+            print("value:", stdout_value)
             if stdout_value.startswith("ok:[localhost] =>"):
                 # Extract the object and remove the last '}'
                 object_start_index = stdout_value.find("{")
