04d4c87f9efc44a4bff2753203bafb37602f6372
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-03 15:05:08 +0200

    trying to retrieve information details about network and security group - 43 attempt

--- ansible-openstack/python-parsing/playbook.py
+++ ansible-openstack/python-parsing/playbook.py
@@ -1,16 +1,23 @@
 # The following code is able to run the Ansible Playbook from this Python script
 
+import json
 import ansible_runner
 r = ansible_runner.run(private_data_dir='/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack', playbook='/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack/playbook.yml')
 
-stdout_list = []  # Initialize an empty list to store stdout values
+stdout_objects = []  # Initialize an empty list to store stdout objects
 for each_host_event in r.events:
     if each_host_event['event'] == "runner_on_ok":
         if 'stdout' in each_host_event:
-            stdout_list.append(each_host_event['stdout'])
+            stdout_value = each_host_event['stdout']
+            if stdout_value.startswith("ok:[localhost] =>"):
+                # Extract the object and remove the last '}'
+                object_start_index = stdout_value.find("{")
+                object_end_index = stdout_value.rfind("}")
+                object_str = stdout_value[object_start_index:object_end_index]
+                stdout_objects.append(json.loads(object_str))
 
-# Now stdout_list contains all stdout values corresponding to events with 'runner_on_ok'
+# Now stdout_objects contains the objects extracted from the second kind of stdout values
 
-# Print each value in stdout_list
-for stdout_value in stdout_list:
-    print(stdout_value)
+# Print each object in stdout_objects
+for obj in stdout_objects:
+    print(obj)
