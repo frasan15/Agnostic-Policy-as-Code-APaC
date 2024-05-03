7389cd5c2cacc262bc275e16a78413a4bf572732
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-03 15:13:54 +0200

    trying to retrieve information details about network and security group - 49 attempt

--- ansible-openstack/python-parsing/playbook.py
+++ ansible-openstack/python-parsing/playbook.py
@@ -11,6 +11,7 @@ for each_host_event in r.events:
             print("i am here")
             stdout_value = each_host_event['stdout']
             print("{}", stdout_value)
+            print(stdout_value.startswith("ok: [localhost] =>"))
             if stdout_value.startswith("ok: [localhost] =>"):
                 print("i am here again")
                 # Extract the object and remove the last '}'
