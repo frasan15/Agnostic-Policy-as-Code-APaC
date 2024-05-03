baee161c816f5e7608a8643e29d8113264365cfa
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-03 18:16:26 +0200

    trying to retrieve information details about network and security group - 113 attempt

--- ansible-openstack/python-parsing/playbook.py
+++ ansible-openstack/python-parsing/playbook.py
@@ -1,11 +1,10 @@
 import ansible_runner
 import json
+import re
 
 r = ansible_runner.run(private_data_dir='/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack', playbook='/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack/playbook.yml')
 
 stdout_objects = []  # Initialize an empty list to store stdout objects
-
-import re
 
 for each_host_event in r.events:
     if each_host_event['event'] == "runner_on_ok": # take only events marked with "runner_on_ok" since the information about network and security group are stored in such events
@@ -14,7 +13,8 @@ for each_host_event in r.events:
             #print("value:", stdout_value)
             # Remove ANSI escape codes from the string -> this escape code makes the json to look green and modify the length of the string causing a lot of issues
             clean_stdout_value = re.sub(r'\x1b\[[0-9;]*m', '', stdout_value)
-            #print("Cleaned value: ", clean_stdout_value)
+            print("Cleaned value: ", clean_stdout_value)
+            print("Repr cleaned value ", repr(clean_stdout_value))
 
             #print(clean_stdout_value.startswith("ok: [localhost]"))
             if clean_stdout_value.startswith("ok: [localhost] =>"):  # These are the only values where the information about network and security group are stored
@@ -23,7 +23,7 @@ for each_host_event in r.events:
                 object_end_index = clean_stdout_value.rfind("}")
                 object_str = clean_stdout_value[object_start_index+1:object_end_index]
                 print("json formatted: ", object_str) # here you can see that the object is no longer green
-                #stdout_objects.append(json.loads(object_str)) # this line doesn't work yet, since there's still some issue with converting the string into json
+                stdout_objects.append(json.loads(object_str)) # this line doesn't work yet, since there's still some issue with converting the string into json
 
 
 # Print each object in stdout_objects
