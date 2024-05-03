208116dc4cafacd7fdee98149a5e6198f823b607
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-03 18:13:01 +0200

    trying to retrieve information details about network and security group - WORKING VERSION: now the strings are stored, although I can't convert them into json yet - 112 attempt

--- ansible-openstack/python-parsing/playbook.py
+++ ansible-openstack/python-parsing/playbook.py
@@ -10,24 +10,21 @@ import re
 for each_host_event in r.events:
     if each_host_event['event'] == "runner_on_ok": # take only events marked with "runner_on_ok" since the information about network and security group are stored in such events
         if 'stdout' in each_host_event: # check if such event has got the stdout field
-            stdout_value = each_host_event['stdout']  # Strip leading and trailing whitespace
+            stdout_value = each_host_event['stdout']  # take the value corresponding to the stdout field
             #print("value:", stdout_value)
-            # Remove ANSI escape codes from the string
+            # Remove ANSI escape codes from the string -> this escape code makes the json to look green and modify the length of the string causing a lot of issues
             clean_stdout_value = re.sub(r'\x1b\[[0-9;]*m', '', stdout_value)
             #print("Cleaned value: ", clean_stdout_value)
 
             #print(clean_stdout_value.startswith("ok: [localhost]"))
-            if clean_stdout_value.startswith("ok: [localhost] =>"):  # Remove space after ":"
-                # Extract the object and remove the last '}'
+            if clean_stdout_value.startswith("ok: [localhost] =>"):  # These are the only values where the information about network and security group are stored
+                # Extract the object and remove the first '{' and the last '}'
                 object_start_index = clean_stdout_value.find("{")
                 object_end_index = clean_stdout_value.rfind("}")
                 object_str = clean_stdout_value[object_start_index+1:object_end_index]
-                print("json formatted: ", object_str)
-                print("object start index: ", object_start_index)
-                print("object end index", object_end_index)
-                stdout_objects.append(json.loads(object_str))
+                print("json formatted: ", object_str) # here you can see that the object is no longer green
+                #stdout_objects.append(json.loads(object_str)) # this line doesn't work yet, since there's still some issue with converting the string into json
 
-# Now stdout_objects contains the objects extracted from the second kind of stdout values
 
 # Print each object in stdout_objects
 for obj in stdout_objects:
