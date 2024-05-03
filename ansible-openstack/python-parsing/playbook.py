efb6a262a7fdd1bcfa2a0f1793b4307f55653881
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-03 18:36:59 +0200

    trying to retrieve information details about network and security group - 123 attempt

--- ansible-openstack/python-parsing/playbook.py
+++ ansible-openstack/python-parsing/playbook.py
@@ -19,13 +19,13 @@ for each_host_event in r.events:
             #print(clean_stdout_value.startswith("ok: [localhost]"))
             if clean_stdout_value.startswith("ok: [localhost] =>"):  # These are the only values where the information about network and security group are stored
                 # Extract the object and remove the first '{' and the last '}'
-                #object_start_index = clean_stdout_value.find("{")
+                object_start_index = clean_stdout_value.find("{")
                 #object_end_index = clean_stdout_value.rfind("}")
-                #object_str = clean_stdout_value[object_start_index+1:object_end_index]
+                object_str = clean_stdout_value[object_start_index:]
                 #new_object_str = object_str.replace('\r', '').replace('\n', '').replace(' ', '')
-                print("json formatted: ", clean_stdout_value) # here you can see that the object is no longer green
+                print("json formatted: ", object_str) # here you can see that the object is no longer green
 
-                stdout_objects.append(json.loads(clean_stdout_value)) # this line doesn't work yet, since there's still some issue with converting the string into json
+                stdout_objects.append(json.loads(object_str)) # this line doesn't work yet, since there's still some issue with converting the string into json
 
 
 # Print each object in stdout_objects
