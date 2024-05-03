db3d5a98a8c53a2510418cc6439f7656af3240c9
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-03 17:19:43 +0200

    trying to retrieve information details about network and security group - 92 attempt

--- ansible-openstack/python-parsing/playbook.py
+++ ansible-openstack/python-parsing/playbook.py
@@ -11,7 +11,7 @@ for each_host_event in r.events:
             stdout_value = each_host_event['stdout'].strip()  # Strip leading and trailing whitespace
             print("value:", stdout_value)
             print(stdout_value.find("b"))
-            print(stdout_value.startswith("1b[0;32mok: [localhost]"))
+            print(stdout_value.startswith("b[0;32mok: [localhost]"))
             if stdout_value.startswith("ok: [localhost]"):  # Remove space after ":"
                 # Extract the object and remove the last '}'
                 object_start_index = stdout_value.find("{")
