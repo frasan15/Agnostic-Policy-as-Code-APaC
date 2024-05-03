ae79a9fcf67fe56ccb7721d081e3b5f78495c1f3
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-03 10:35:21 +0200

    trying to retrieve information details about network and security group - 30 attempt

--- ansible-openstack/python-parsing/playbook.py
+++ ansible-openstack/python-parsing/playbook.py
@@ -2,7 +2,7 @@
 
 import ansible_runner
 r = ansible_runner.run(private_data_dir='/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack', playbook='/home/ubuntu/Verification-and-Validation-of-IaC/ansible-openstack/playbook.yml')
-print("{}: {}".format(r.status, r.rc))
+print("this is the format: {}: {}".format(r.status, r.rc))
 # successful: 0
 for each_host_event in r.events:
     print(each_host_event['event'])
