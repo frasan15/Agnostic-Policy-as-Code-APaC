d687c18d1b2c7d58ad99a49c24fa0085f62bc0bf
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-09 18:08:10 +0200

    retrieving information about networks, servers, subnets, default groups from OpenStack in Terraform - 24 attempt

--- terraform-openstack/output.tf
+++ terraform-openstack/output.tf
@@ -1,4 +1,7 @@
-
+# Output the server instance IDs
+output "server_instance_ids" {
+  value = locals.server_instance_ids
+}
 
 #output "server_info" {
 #    value = data.openstack_compute_instance_v2.instance
