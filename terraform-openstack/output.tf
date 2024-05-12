20ecf2cedf6cc68d40dee8d09bc59831d0031e92
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-12 17:27:55 +0200

    generating generic input from Terraform-OpenStack - 1 attempt

--- terraform-openstack/output.tf
+++ terraform-openstack/output.tf
@@ -13,6 +13,12 @@ output "server_info_2" {
     value = data.openstack_compute_instance_v2.server_info_2
 }
 
+output "servers" {
+  value = [data.openstack_compute_instance_v2.server_info,
+  data.openstack_compute_instance_v2.server_info_2
+  ]
+}
+
 output "network_info" {
   value = data.openstack_networking_network_v2.network
 }
