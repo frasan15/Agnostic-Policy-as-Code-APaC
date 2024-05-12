460e9a7308fcfff53961bbebc1fac27103bb2487
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-12 17:44:46 +0200

    generating generic input from Terraform-OpenStack - 3 attempt

--- terraform-openstack/output.tf
+++ terraform-openstack/output.tf
@@ -1,32 +1,29 @@
 # Print the ids's list -> there's still the \n problem to fix
-output "server_instance_ids" {
-  value = local.server_instance_ids
-}
-
-# Output the server instance IDs
-output "server_info" {
-    value = data.openstack_compute_instance_v2.server_info
-}
-
-# Output the server instance IDs
-output "server_info_2" {
-    value = data.openstack_compute_instance_v2.server_info_2
-}
+# output "server_instance_ids" {
+#  value = local.server_instance_ids
+#}
 
 output "servers" {
-  value = [data.openstack_compute_instance_v2.server_info,
-  data.openstack_compute_instance_v2.server_info_2
+  value = [
+    data.openstack_compute_instance_v2.server_info,
+    data.openstack_compute_instance_v2.server_info_2
   ]
 }
 
-output "network_info" {
-  value = data.openstack_networking_network_v2.network
+output "networks" {
+  value = [
+    data.openstack_networking_network_v2.network
+  ]
 }
 
-output "security_group_info" {
-  value = local.secgroup_info
+output "security_groups" {
+  value = [
+    local.secgroup_info
+  ]
 }
 
-output "subnet_info" {
-  value = data.openstack_networking_subnet_v2.subnet_1
+output "subnets" {
+  value = [
+    data.openstack_networking_subnet_v2.subnet_1
+  ]
 }
