d9006ee22d3cd8331b81287a8ef45419bfb3aaac
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-13 15:00:19 +0200

    adding router and floating ip output information - 17 attempt

--- terraform-openstack/main.tf
+++ terraform-openstack/main.tf
@@ -54,7 +54,7 @@ resource "openstack_compute_instance_v2" "web_server" {
 resource "openstack_networking_floatingip_v2" "myip"{
   depends_on = [ openstack_compute_instance_v2.web_server ]
   pool = "ntnu-internal"
-  port_id = openstack_compute_instance_v2.web_server
+  #port_id = openstack_compute_instance_v2.web_server
   #fixed_ip = openstack_compute_instance_v2.web_server.access_ip_v4
 }
 
