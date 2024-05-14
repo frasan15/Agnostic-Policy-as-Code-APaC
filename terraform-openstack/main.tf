f801f680e1fa93ad493ed18bb08d62a5d8619146
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-14 15:55:31 +0200

    generating infrastructure from scratch from Terraform to OpenStack; implementing version where it's not needed to execute terraform itself, the tf file is checked directly- 1 attempt

--- terraform-openstack/main.tf
+++ terraform-openstack/main.tf
@@ -14,22 +14,22 @@ terraform {
 
 # Create a network
 resource "openstack_networking_network_v2" "network_1" {
-  name = var.network1
+  name = "network1"
   admin_state_up = "true"
 }
 
 # Create a subnet within the previously created network
 resource "openstack_networking_subnet_v2" "subnet_1" {
-  name = var.subnet1
+  name = "subnet1"
   network_id = openstack_networking_network_v2.network_1.id
   cidr = "192.168.111.0/24"
   ip_version = 4
 }
 
-# Define a security group which exposes port 22
+# Define a security group which exposes port 80
 resource "openstack_networking_secgroup_v2" "secgroup_1" {
-  name        = var.security_groups
-  description = "Expose port 22"
+  name        = "secgroup_1"
+  description = "Expose port 80" # remember to change this if you modify the rules
 }
 
 # Specifically, you define the rules hereby
@@ -39,17 +39,32 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_1" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_1.id
+}
+
+resource "openstack_networking_secgroup_v2" "secgroup_2" {
+  name = "secgroup_2"
+  description = "Expose port 22"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_2" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
   port_range_min    = 22
   port_range_max    = 22
   remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.secgroup_1.id
+  security_group_id = openstack_networking_secgroup_v2.secgroup_2.id
 }
 
 # Define a port (or interface) to connect the server to the newly created subnet; defining a port means 
 # to specify the subnet id where to connect the server to, and the ip address this interface will have
 # this ip address is basically the ip address of the server itself
-resource "openstack_networking_port_v2" "port_1" {
-  name = "port_1"
+resource "openstack_networking_port_v2" "port_server_1" {
+  name = "port_server_1"
   network_id = openstack_networking_network_v2.network_1.id
   admin_state_up = "true"
   security_group_ids = [openstack_networking_secgroup_v2.secgroup_1.id]
@@ -60,29 +75,54 @@ resource "openstack_networking_port_v2" "port_1" {
   }
 }
 
-# Create a web server instance
+resource "openstack_networking_port_v2" "port_server_2" {
+  name = "port_server_2"
+  network_id = openstack_networking_network_v2.network_1.id
+  admin_state_up = "true"
+  security_group_ids = [openstack_networking_secgroup_v2.secgroup_2.id]
+
+  fixed_ip {
+    subnet_id = openstack_networking_subnet_v2.subnet_1.id
+    ip_address = "192.168.111.11"
+  }
+}
+
+# Create the first server instance which exposes port 80
 resource "openstack_compute_instance_v2" "web_server" {
   depends_on = [ openstack_networking_secgroup_rule_v2.secgroup_rule_1 ]
-  name            = var.server_name
+  name            = "server1"
   flavor_name     = "gx1.2c4r"
   image_id        = "db1bc18e-81e3-477e-9067-eecaa459ec33"
   network {
-    port = openstack_networking_port_v2.port_1.id
+    port = openstack_networking_port_v2.port_server_1.id
   }
   security_groups = [openstack_networking_secgroup_v2.secgroup_1.name]
   key_pair = "MySecondKey"
 
 # The following provisioner writes into the file server_ids.txt the id of the newly created server (N.B. I will use this file as a list of all the server's ids created)
-# TODO: fix the \n problem
   provisioner "local-exec" {
     command = "echo '\n' >> server_ids.txt; echo '${self.id}' >> server_ids.txt"
   }
 
 }
 
+# Create the second server instance which exposes port 22
+resource "openstack_compute_instance_v2" "web_server" {
+  depends_on = [ openstack_networking_secgroup_rule_v2.secgroup_rule_2 ]
+  name            = "server2"
+  flavor_name     = "gx1.2c4r"
+  image_id        = "db1bc18e-81e3-477e-9067-eecaa459ec33"
+  network {
+    port = openstack_networking_port_v2.port_server_2.id
+  }
+  security_groups = [openstack_networking_secgroup_v2.secgroup_2.name]
+  key_pair = "MySecondKey"
+
+}
+
 # Define a router -> among the other things, this is needed to generate a floating ip from the right pool, since the router will be connected to ntnu-internal network
 resource "openstack_networking_router_v2" "router_1" {
-  name = var.router1
+  name = "router1"
   admin_state_up = "true"
   external_network_id = "730cb16e-a460-4a87-8c73-50a2cb2293f9"
 }
@@ -97,7 +137,7 @@ resource "openstack_networking_router_interface_v2" "router_interface_1" {
 resource "openstack_networking_floatingip_v2" "myip"{
   depends_on = [ openstack_compute_instance_v2.web_server, openstack_networking_router_interface_v2.router_interface_1 ]
   pool = "ntnu-internal"
-  port_id = openstack_networking_port_v2.port_1.id
+  port_id = openstack_networking_port_v2.port_server_1.id
 }
 
 # Define all the information needed for the subnet here below
@@ -161,12 +201,12 @@ data "openstack_compute_instance_v2" "server_info_2" {
 
 data "openstack_networking_network_v2" "network" {
   depends_on = [ openstack_networking_network_v2.network_1 ]
-  name = var.network1
+  name = "network1"
 }
 
 data "openstack_networking_secgroup_v2" "secgroup" {
   depends_on = [ openstack_networking_secgroup_rule_v2.secgroup_rule_1 ]
-  name = var.security_groups
+  name = "secgroup_1"
 }
 
 data "openstack_networking_subnet_v2" "subnet_1" {
@@ -176,7 +216,7 @@ data "openstack_networking_subnet_v2" "subnet_1" {
 
 data "openstack_networking_router_v2" "router" {
   depends_on = [ openstack_networking_router_v2.router_1 ]
-  name = var.router1
+  name = "router1"
 }
 
 # ERROR: The following module is shown in the documentation but it's not found once terraform apply is executed
