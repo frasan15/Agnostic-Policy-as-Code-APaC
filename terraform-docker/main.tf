9404baa7ea136e81c8ca8fd5906da8224333d018
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-05-16 16:53:58 +0200

    Creating infrastructure on Docker from Terraform - 9 attempt

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -17,7 +17,7 @@ resource "docker_image" "nginx" {
 # Define a Docker network
 resource "docker_network" "network1" {
   name   = "network1"
-  driver = "host"
+  driver = "overlay"
   ipam_config {
     subnet = "192.168.111.0/24"
   }
