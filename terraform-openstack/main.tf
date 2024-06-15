cf5598979839397353ad4cfaff90b4cc1f883e31
Francesco Santoro s309482 <s309482@studenti.polito.it>
2024-06-15 11:13:51 +0200

    server3 added to each implementation; docker deployments can now detect whether the server is reachable from outside - 3 attempt

--- terraform-openstack/main.tf
+++ terraform-openstack/main.tf
@@ -72,7 +72,7 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_3" {
   port_range_min    = 443
   port_range_max    = 443
   remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.secgroup_2.id
+  security_group_id = openstack_networking_secgroup_v2.secgroup_3.id
 }
 
 # Define a port (or interface) to connect the server to the newly created subnet; defining a port means 
